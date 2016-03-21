d05dba50fb7a750275a103f20615e2c38ff0ccb9
John Murray <me@johnmurray.io>
2016-03-21 08:06:25 -0400

    Updated etcd launcher
    now works for both a local dev environment and the test (TravisCI)
    environment.

--- test-launch-etcd.sh
+++ test-launch-etcd.sh
@@ -11,19 +11,35 @@ fi
 
 echo "Using ETCD version $ETCD_VERSION"
 
-git clone https://github.com/coreos/etcd.git
-cd etcd
-git checkout $ETCD_VERSION
-./build
+if [[ ! -e etcd ]] ; then
+  rm -rf etcd
+  git clone --depth=1 https://github.com/coreos/etcd.git etcd
+  cd etcd
+  git checkout $ETCD_VERSION
+  echo 'building etcd (may take a while)...'
+  ./build
+else
+  cd etcd
+fi
 
 # setup go
-export GOPATH='/tmp/gopath'
-mkdir -p /tmp/gopath
+if [[ ! $GOPATH ]] ; then
+  echo 'Setting up GOPATH'
+  export GOPATH='/tmp/gopath'
+  mkdir -p /tmp/gopath
+fi
 
 # install goreman and run etcd
-go get -u github.com/mattn/goreman
-nohup $GOPATH/bin/goreman start >etcd.log 2>&1 &
-sleep 5
+if [[ $TRAVIS ]] ; then
+  echo 'installing goreman'
+  go get -u github.com/mattn/goreman
+  echo 'starting etcd'
+  nohup $GOPATH/bin/goreman start >etcd.log 2>&1 &
+  sleep 5
+else
+  echo 'starting etcd'
+  $GOPATH/bin/goreman start
+fi
 
 ${TRAVIS:?"This is not a Travis build. All Done"}
 #Temporal solution to travis issue #155
